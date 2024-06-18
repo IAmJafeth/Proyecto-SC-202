1151771544b58f166ee548a6416e0a4eb21380f7
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2024-06-17 20:03:45 -0600

    Create Asiento.java

---
+++ src/main/java/com/grupo1/proyecto/Asiento.java
@@ -0,0 +1,41 @@
+package com.grupo1.proyecto;
+
+public class Asiento {
+    private String pasajero;
+    private int numeroAsiento;
+    private int numeroVuelo;
+
+    public Asiento(String pasajero, int numeroAsiento, int numeroVuelo){
+        this.pasajero = pasajero;
+        this.numeroAsiento = numeroAsiento;
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public boolean isDisponible() {
+        return pasajero == null;
+    }
+
+    public String getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(String pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public void setNumeroAsiento(int numeroAsiento) {
+        this.numeroAsiento = numeroAsiento;
+    }
+
+    public int getNumeroVuelo() {
+        return numeroVuelo;
+    }
+
+    public void setNumeroVuelo(int numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+}
